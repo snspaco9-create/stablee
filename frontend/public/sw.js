b51c3bbf88cde451c516b6d2887b496a6dffe53c
snspaco9-create <snspaco9@gmail.com>
2026-05-23 22:47:09 +0100

    fix service worker caching

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,19 +1,23 @@
-const CACHE_NAME = 'stablee-v1'
-const urlsToCache = [
-  '/',
-  '/index.html'
-]
+const CACHE_NAME = 'stablee-v2'
 
 self.addEventListener('install', event => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
   )
 })
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request)
-    })
-  )
+  if (event.request.method !== 'GET') return
+  
+  if (event.request.url.includes('/api/')) {
+    return
+  }
+
+  event.respondWith(fetch(event.request).catch(() => caches.match('/')))
 })
