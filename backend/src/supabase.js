05ed4261014d51b604e60c91f5262c0c2b2e50d0
snspaco9-create <snspaco9@gmail.com>
2026-06-06 22:07:51 +0100

    corrected error in supabase.js

--- backend/src/supabase.js
+++ backend/src/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 )
 
 export default supabase
